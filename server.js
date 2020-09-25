2bfc5d8a46ccd11073c96126d4f3ed828f5dd425
Sam Zyra <sisaaczyra@gmail.com>
2020-09-25 10:56:38 -0700

    Created last routes and commented out next steps.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 
+
 const app = express();
 const PORT = 8080;
+
+const noteData = [];
 
 app.get("/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "notes.html"));
@@ -16,12 +19,25 @@ app.get("/api/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "db.json"));
 });
 
+// readFileAsync (filename, json)
+
 app.post("/api/notes", function(req,res) {
-    
+    const newNote = req.body;
+    // Use FS to read db.json
+    // Use JSON.parse to get result (array) in JS
+    // push new note to result array
+    // JSON.stringify array
+    //Use FS to write this in db.json
 });
 
 app.delete("/api/notes/:id", function(req,res) {
-
+    const noteID = req.params.id
+    // Use FS to read db.json
+    // use JSON.parse to get result array in JS
+    // Loop over aray to find matching id (noteID)
+    // Remove note with matching id from array
+    // JSON.stringify to turn back to json
+    // Use FS to write to db.json
 });
 
 app.listen(PORT, function() {
