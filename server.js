6f66f6eb786134d9619977ecf6b00422b48ac786
Sam Zyra <sisaaczyra@gmail.com>
2020-09-25 21:42:34 -0700

    Progress on api routes.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
 
 const app = express();
 const PORT = 8080;
-
-const noteData = [];
 
 app.get("/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "notes.html"));
@@ -24,17 +22,29 @@ app.get("/api/notes", function(req,res) {
 app.post("/api/notes", function(req,res) {
     const newNote = req.body;
     // Use FS to read db.json
+    let read = fs.readFileSync("db.json")
     // Use JSON.parse to get result (array) in JS
+    let array = JSON.parse(read);
     // push new note to result array
+    newNote.push(array);
     // JSON.stringify array
+    let string = JSON.stringify(array);
     //Use FS to write this in db.json
+    fs.writeFileSync("db.json", string)
 });
 
 app.delete("/api/notes/:id", function(req,res) {
     const noteID = req.params.id
     // Use FS to read db.json
+    let read = fs.readFileSync("db.json");
     // use JSON.parse to get result array in JS
+    let array = JSON.parse(read);
     // Loop over aray to find matching id (noteID)
+    for (i = 0; i < parse.length; i++) {
+        if(parse[i] == noteID){
+
+        }
+    }
     // Remove note with matching id from array
     // JSON.stringify to turn back to json
     // Use FS to write to db.json
